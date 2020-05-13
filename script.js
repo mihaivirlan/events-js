25f6f7e83036dd2f957693f63559b76ca427868a
mihaivirlan <mihaivirlan93@gmail.com>
2020-05-14 00:30:19 +0300

    Event Object Properties

--- script.js
+++ script.js
@@ -23,15 +23,47 @@
 //     console.log('Clicked outer!');
 // }
 
-var inner = document.querySelector('#inner');
-var outer = document.querySelector('#outer');
-inner.addEventListener('click', innerListener);
-outer.addEventListener('click', outerListener);
-function innerListener(event) {
-    console.log(event.bubbles);
-    event.stopPropagation();
-    console.log('Clicked inner!');
-};
-function outerListener(event) {
-    console.log('Clicked outer!');
-}
+// var inner = document.querySelector('#inner');
+// var outer = document.querySelector('#outer');
+// inner.addEventListener('click', innerListener);
+// outer.addEventListener('click', outerListener);
+// function innerListener(event) {
+//     console.log(event.bubbles);
+//     event.stopPropagation();
+//     console.log('Clicked inner!');
+// };
+// function outerListener(event) {
+//     console.log('Clicked outer!');
+// }
+
+
+
+    //Event Object Properties
+// var inner = document.querySelector('#inner');
+// var outer = document.querySelector('#outer');
+// inner.addEventListener('click', innerListener);
+// outer.addEventListener('click', outerListener);
+// function innerListener(event) {
+//     console.log(event.target);
+//     event.target.style.backgroundColor = 'red';
+//     event.stopPropagation();
+//     console.log('Clicked inner!');
+// };
+// function outerListener(event) {
+//     console.log(event.target);
+//     event.target.style.backgroundColor = 'blue';
+//     console.log('Clicked outer!');
+// }
+
+// var inner = document.querySelector('#inner');
+// var outer = document.querySelector('#outer');
+// inner.addEventListener('click', innerListener);
+// outer.addEventListener('click', outerListener);
+// function innerListener(event) {
+//     console.log(event.clientX, event.clientY);
+//     event.stopPropagation();
+//     console.log('Clicked inner!');
+// };
+// function outerListener(event) {
+//     console.log('Clicked outer!');
+// }
