79bca6926398d20e28dc995287562caed6fa4ec7
mihaivirlan <mihaivirlan93@gmail.com>
2020-05-13 20:21:44 +0300

    Event Behavior

--- script.js
+++ script.js
@@ -1,81 +1,37 @@
-    //Event Handlers
-// window.onload = function() {
-//     console.log('Window loaded!');
+//     //Event Behavior
+// var inner = document.querySelector('#inner');
+// var outer = document.querySelector('#outer');
+// inner.addEventListener('click', innerListener);
+// outer.addEventListener('click', outerListener);
+// function innerListener(event) {
+//     console.log('Clicked inner!');
 // };
-
-// var btn = document.querySelector('button');
-// btn.onclick = function() {
-//     console.log('Clicked');
-// };
-// btn.onclick = function() {
-//     console.log('Clicked');
-// };
-
-// var btn = document.querySelector('button');
-// btn.onclick = function() {
-//     console.log('Clicked');
-// };
-// btn.onmouseover = function() {
-//     console.log('Clicked');
-// };
-
-// var btn = document.querySelector('button');
-// btn.onclick = function() {
-//     console.log('Button clicked');
-// };
-// btn.onmouseover = function() {
-//     console.log('Button entered');
-// };
-// btn.onmouseleave = function() {
-//     console.log('Button lived');
-// };
-
-// var btn = document.querySelector('button');
-// document.onclick = function() {
-//     console.log('Button clicked');
-// };
-
-// var btn = document.querySelector('button');
-// btn.onclick = function() {
-//     console.log('Button clicked');
-// };
-// btn.onclick = function() {
-//     console.log('Also clicked');
-// };
-// //Second btn.onclick method overwrite first method - and that is a problem because often times we will need multiple listeners and we don't want only one handler!
-
-
-
-    //Event Listeners
-// var btn = document.querySelector('button');
-// btn.addEventListener('click', listener1);
-// function listener1() {
-//     console.log('Listener1');
+// function outerListener(event) {
+//     console.log('Clicked outer!');
 // }
-// function listener2() {
-//     console.log('Listener2'); 
+// //In this exampple if we click on yellow div, we can see in console log output both output's, in this case for bellow example will use the stop event propagation method!
+
+// var inner = document.querySelector('#inner');
+// var outer = document.querySelector('#outer');
+// inner.addEventListener('click', innerListener);
+// outer.addEventListener('click', outerListener);
+// function innerListener(event) {
+//     event.stopPropagation();
+//     console.log('Clicked inner!');
+// };
+// function outerListener(event) {
+//     console.log('Clicked outer!');
 // }
 
-// var btn = document.querySelector('button');
-// btn.addEventListener('click', listener1);
-// btn.addEventListener('click', listener2);
-// function listener1() {
-//     console.log('Listener1');
-// }
-// function listener2() {
-//     console.log('Listener2'); 
-// }
-
-// var btn = document.querySelector('button');
-// btn.addEventListener('click', listener1);
-// btn.addEventListener('click', listener2);
-// setTimeout(function() {
-//     btn.removeEventListener('click', listener1);
-// }, 2000);
-// function listener1() {
-//     console.log('Listener1');
-// }
-// function listener2() {
-//     console.log('Listener2'); 
-// }
-
+var inner = document.querySelector('#inner');
+var outer = document.querySelector('#outer');
+inner.addEventListener('click', innerListener);
+outer.addEventListener('click', outerListener);
+function innerListener(event) {
+    console.log(event.bubbles);
+    event.stopPropagation();
+    console.log('Clicked inner!');
+};
+function outerListener(event) {
+    console.log('Clicked outer!');
+}
